77d94a29a07e7c01eb177cc913de4d916d6c775b
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-04-25 13:56:04 -0700

    fizzbuzz

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -4,16 +4,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
-        System.out.print("Principle: ");
-         Scanner scanner = new Scanner(System.in);
-         int principle = scanner.nextInt();
-        System.out.print("Interest Rate: ");
-        float interest = scanner.nextFloat() / PERCENT / MONTHS_IN_YEAR;
-        System.out.print("Term in Years: ");
-        int payments = scanner.nextInt() * MONTHS_IN_YEAR;
-        double paymentAmount = principle * ((interest * Math.pow(1+ interest, payments)) / (Math.pow(1+interest, payments) -1));
-        System.out.println(paymentAmount);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number: ");
+        int number = scanner.nextInt();
+        String output = "";
+
+        if (number % 5 == 0)
+            output += "Fizz";
+        if (number % 3 == 0)
+            output += "Buzz";
+        if (output.equals(""))
+            output = String.valueOf(number);
+
+        System.out.println(output);
     }
 }
