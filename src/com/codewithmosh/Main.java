07a9a1c0ea8ed1a87a98e9e0e56c179d7b294de4
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-04-25 13:13:11 -0700

    mortgage calculator

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -4,9 +4,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Age: 20");
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+        System.out.print("Principle: ");
          Scanner scanner = new Scanner(System.in);
-         byte age = scanner.nextByte();
-        System.out.println("You are " + age);
+         int principle = scanner.nextInt();
+        System.out.print("Interest Rate: ");
+        float interest = scanner.nextFloat() / PERCENT / MONTHS_IN_YEAR;
+        System.out.print("Term in Years: ");
+        int payments = scanner.nextInt() * MONTHS_IN_YEAR;
+        double paymentAmount = principle * ((interest * Math.pow(1+ interest, payments)) / (Math.pow(1+interest, payments) -1));
+        System.out.println(paymentAmount);
     }
 }
