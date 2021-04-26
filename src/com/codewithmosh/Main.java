cf3060940ef41477cdfff5caac1990a07f7ec3b6
Paul Stevens <66223664+Protopaco@users.noreply.github.com>
2021-04-26 11:15:59 -0700

    mortgage calculator with error handling

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,21 +1,46 @@
 package com.codewithmosh;
+import java.util.Locale;
 import java.util.Scanner;
+import java.text.NumberFormat;
 
 public class Main {
 
     public static void main(String[] args) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+        float[] principleConditions = new float[]{0F, 100000000F};
+        int principle = Math.round(input("Amount borrowed: ", principleConditions));
+
+        float[] interestConditions = new float[]{0F, 100F};
+        float interestResponse = input("Interest Rate: ", interestConditions);
+        float interest = interestResponse / PERCENT / MONTHS_IN_YEAR;
+
+        float[] termConditions = new float[]{0F, 100F};
+        int term = Math.round(input("Term in Years: ", termConditions));
+        int termInMonths = term * MONTHS_IN_YEAR;
+
+        double paymentAmount = principle * ((interest * Math.pow(1+ interest, termInMonths)) / (Math.pow(1+interest, termInMonths) -1));
+        String displayAmount = formatToDollars(paymentAmount);
+
+        System.out.println("Payment: " + displayAmount);
+
+    }
+
+    private static Float input(String prompt, float[] conditions ) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Number: ");
-        int number = scanner.nextInt();
-        String output = "";
+        float response;
 
-        if (number % 5 == 0)
-            output += "Fizz";
-        if (number % 3 == 0)
-            output += "Buzz";
-        if (output.equals(""))
-            output = String.valueOf(number);
+        do{
+            System.out.print(prompt);
+            response = scanner.nextFloat();
+        }
 
-        System.out.println(output);
+        while ((response < conditions[0]) && (response > conditions[1]));
+        return response;
+    }
+
+    private static String formatToDollars(double input) {
+        NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
+        return us.format(input);
     }
 }
